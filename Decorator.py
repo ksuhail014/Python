5635d29dd7a4b85b449a4e4090afcd05157ff3cd
ksuhail014 <ksuhail014@gmail.com>
2024-05-23 10:55:49 +0530

    Add files via upload

---
+++ Decorator.py
@@ -0,0 +1,34 @@
+def my_decorator(func):
+    def wrapper():
+        print("Something is happening before the function is called.")
+        func()
+        print("Something is happening after the function is called.")
+    return wrapper
+
+@my_decorator
+def say_hello():
+    print("Hello!")
+
+say_hello()
+
+# 2.
+class MyClass:
+    def __init__(self, value):
+        self._value = value
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, new_value):
+        if new_value >= 0:
+            self._value = new_value
+        else:
+            raise ValueError("Value must be non-negative.")
+
+obj = MyClass(10)
+print(obj.value)
+obj.value = 20
+print(obj.value)
+
